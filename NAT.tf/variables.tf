3feef431b1de112d7e45c344c8d9f0d20ca5b553
dsweil <derrick.weil@hotmail.com>
2024-11-29 05:04:31 +0700

    Delete NAT.tf directory

--- NAT.tf/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs where NAT Gateways will be created"
-  type        = list(string)
-}
-
-variable "private_route_table_ids" {
-  description = "List of private route table IDs to associate with the NAT Gateway"
-  type        = list(string)
-}
